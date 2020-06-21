8c2a035716de818283f7ced23ef989e0ea7f393b
chrisloarryn <chrisloarryn@gmail.com>
2020-06-20 23:40:48 -0400

    form and list

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,105 @@
 <script>
-	export let name;
+  let products = [
+    {
+      id: 1,
+      name: 'HP Pavilion Notebook',
+      description: 'HP Laptop',
+      category: 'laptop',
+    },
+    {
+      id: 2,
+      name: 'Mouse Razer',
+      description: 'Razer Gaming Mouse',
+      category: 'Peripherals',
+    },
+  ]
+  let product = {
+    id: '',
+    name: '',
+    description: '',
+    category: '',
+    imageURL: '',
+  }
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+    console.log(product)
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
+
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        {#each products as product}
+          <div class="card mt-2">
+            <div class="row">
+              <div class="col-md-4">
+                <img src="" alt="" />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5>
+                    <strong>{product.name}</strong>
+                    <span>
+                      <small>{product.category}</small>
+                    </span>
+                  </h5>
+                  <p class="card-text">{product.description}</p>
+                  <button class="btn btn-danger">Delete</button>
+                  <button class="btn btn-secondary">Edit</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        {/each}
+      </div>
+      <div class="col-md-6">
+        <div class="card-body">
+          <form on:submit={onSubmitHandler}>
+            <div class="form-group">
+              <input
+                bind:value={product.name}
+                type="text"
+                placeholder="Product Name"
+                id="productName"
+                class="form-control" />
+            </div>
+            <div class="form-group">
+              <textarea
+                bind:value={product.description}
+                id="productDescription"
+                placeholder="Product Description"
+                rows="3"
+                class="form-control" />
+            </div>
+            <div class="form-group">
+              <input
+                bind:value={product.imageURL}
+                type="url"
+                placeholder="https://github.com/chrisloarryn"
+                id="productImageUrl"
+                class="form-control" />
+            </div>
+            <div class="form-group">
+              <select
+                bind:value={product.category}
+                id="cat"
+                name="cat"
+                class="form-control">
+                <option value="laptops" selected>Laptops</option>
+                <option value="peripherals">Peripherals</option>
+                <option value="servers">Servers</option>
+              </select>
+            </div>
+            <button class="btn btn-secondary">Save Product</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
